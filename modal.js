c8cb88ff701496683200321a9d5b73c10aeee130
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2023-10-12 17:50:52 +0200

    comments in modal.js

--- modal.js
+++ modal.js
@@ -21,3 +21,31 @@ function launchModal() {
 }
 
 
+// Data recuperation //
+
+  //do a common Regex for first & last name
+    // First name = fonction catch the entry with a querySelector & put if(regex ok = true)/else = false 
+    // Last name = same
+    
+    //Email = function => catch the entry regex email test => if/else
+
+    //Birthday = faire un check si no valeur = error | use a date regex
+    //tournaments = faire un check si no valeur = error | num regex
+
+    // radio city search how to listen radio & error if no selection
+    // checkbox use condition check si check on if not => error
+    // checkbox ad listen to get the data
+
+// IF ALL TRUE = collecting value //
+
+  //create on object from the data
+  // send the data in Local Storage (JSON.stringify)
+
+// User Friendly//
+
+  // maybe put a popup to tell the form is send
+
+
+// /!\ TO FIX /!\ //
+
+  // cross to close the form don't work
